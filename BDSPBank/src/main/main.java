09ec86585e88cae92432b31732ca132ecccb7b68
Bruno Da Silva Pinto <brunodspinto98@gmail.com>
2024-10-10 10:37:42 +0200

    Table account dans main

--- BDSPBank/src/main/main.java
+++ BDSPBank/src/main/main.java
@@ -3,7 +3,10 @@ package main;
 import java.util.ArrayList;
 import java.util.List;
 
+import components.Account;
 import components.Client;
+import components.CurrentAccount;
+import components.SavingsAccount;
 
 public class main {
 	
@@ -22,14 +25,44 @@ public class main {
 	
 	
 	public static void displayClients(List<Client> clients) {
-		System.out.println(clients.toString());
+		clients.stream()
+        		.map(Client::toString)
+        		.forEach(System.out::println);
 	}
 	
+	
+	// 1.2.3 Creation of the table account
+	
+	public static List<Account> generateAccounts(List<Client> clients){
+		
+		List<Account> accounts = new ArrayList<>();
+		
+		for (Client client : clients) {
+			accounts.add(new CurrentAccount(client));
+			accounts.add(new SavingsAccount(client));
+		}
+		
+		return accounts;
+	}
+	
+	
+	public static void displayAccounts(List<Account> accounts) {
+        accounts.stream()
+                .map(Account::toString)
+                .forEach(System.out::println);
+    }
+	
+	
+	// Main
 
 	public static void main(String[] args) {
 		List<Client> clients = generateClients(3);
 
 		displayClients(clients);
+		
+		List<Account> accounts = generateAccounts(clients);
+		
+		displayAccounts(accounts);
 	}
 
 }
